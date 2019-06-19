df7705d462b00320a163bd147b758f7cdfc4bbe8
AterCorvus <gerbov94@gmail.com>
2019-06-19 21:48:31 +0300

    Home 19.06.19
    remove and update finish

--- 19.06.19MyLinkedList/src/model/MyLinkedList.java
+++ 19.06.19MyLinkedList/src/model/MyLinkedList.java
@@ -5,19 +5,18 @@ public class MyLinkedList<T> implements MyList<T> {
 	private Node<T> head = null;
 	private Node<T> tail = null;
 	private int size = 0;
-	
+
 	@Override
 	public void add(T data) {
 		Node<T> node = new Node<T>(data);
-		
+
 		if (head == null) {
 			head = node;
-		}
-		else {
+		} else {
 			node.setPrev(tail);
 			tail.setNext(node);
 		}
-		
+
 		tail = node;
 		size++;
 	}
@@ -25,15 +24,14 @@ public class MyLinkedList<T> implements MyList<T> {
 	@Override
 	public void addFirst(T data) {
 		Node<T> node = new Node<T>(data);
-		
+
 		if (head == null) {
 			tail = node;
-		}
-		else {
+		} else {
 			node.setNext(head);
 			head.setPrev(node);
 		}
-		
+
 		head = node;
 		size++;
 	}
@@ -45,52 +43,65 @@ public class MyLinkedList<T> implements MyList<T> {
 
 	@Override
 	public boolean addByIndex(int index, T data) {
-		if(index < 0 || index > size)
+		if (index < 0 || index > size)
 			return false;
-		if (index == 0) { 
+		if (index == 0) {
 			addFirst(data);
-		}
-		else if(index == size) {
+		} else if (index == size) {
 			add(data);
-		}
-		else {
+		} else {
 			Node<T> node = new Node<T>(data);
 			Node<T> target = getNodeByIndex(index);
 			Node<T> prev = target.getPrev();
-			
+
 			node.setNext(target);
 			node.setPrev(prev);
 			prev.setNext(node);
 			target.setPrev(node);
-			
+
 			size++;
 		}
-		
+
 		return true;
 	}
 
 	private Node<T> getNodeByIndex(int index) {
-		Node<T> result = head;
-		int counter = 0;
+		if (index < 0 || index > size)
+			return null;
 		
-		while(counter < index) {
-			result = result.getNext();
-			counter++;
+		Node<T> result = null;
+		if (index < (size / 2)) {
+			result = head;
+			int counter = 0;
+
+			while (counter < index) {
+				result = result.getNext();
+				counter++;
+			}
+		} else {
+			result = tail;
+			int counter = size - 1;
+
+			while (counter > index) {
+				result = result.getPrev();
+				counter--;
+			}
 		}
+
 		return result;
 	}
 
 	@Override
 	public int indexOf(T data) {
-		if(head == null) 
+		if (head == null)
 			return -1;
-		
+
 		int counter = 0;
 		for (Node<T> node = head; node != null; node = node.getNext(), counter++) {
 			if (node.getData().equals(data))
 				return counter;
 		}
-		
+
 		return -1;
 	}
 
@@ -121,39 +132,116 @@ public class MyLinkedList<T> implements MyList<T> {
 	public boolean set(int index, T data) {
 		if (index < 0 || index >= size)
 			return false;
-		
+
 		getNodeByIndex(index).setData(data);
 		return true;
 	}
 
+	private T removeLastOne(Node<T> node) {
+		if (node == null)
+			return null;
+
+		head = null;
+		tail = null;
+		size = 0;
+		return node.getData();
+	}
+
 	@Override
 	public T removeFirst() {
-		// TODO Auto-generated method stub
-		return null;
+		if (head == null)
+			return null;
+		else if (head == tail) {
+			return removeLastOne(head);
+		} else {
+			Node<T> node = head;
+			head = node.getNext();
+			head.setPrev(null);
+			size--;
+			return node.getData();
+		}
 	}
 
 	@Override
 	public T removeLast() {
-		// TODO Auto-generated method stub
-		return null;
+		if (head == null)
+			return null;
+		else if (head == tail) {
+			return removeLastOne(head);
+		} else {
+			Node<T> node = tail;
+			tail = node.getPrev();
+			tail.setNext(null);
+			size--;
+			return node.getData();
+		}
 	}
 
 	@Override
 	public T remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if (index < 0 || index >= size)
+			return null;
+		else if (index == 0) {
+			return removeFirst();
+		} else if (index == size) {
+			return removeLast();
+		}
+
+		Node<T> target = getNodeByIndex(index);
+		if (target == null)
+			return null;
+
+		return remove(target);
+	}
+
+	private T remove(Node<T> node) {
+		if (node == null) {
+			return null;
+		} else if (head == tail) {
+			return removeLastOne(head);
+		} else {
+			Node<T> prev = node.getPrev();
+			Node<T> next = node.getNext();
+
+			if (next == null)
+				return removeLast();
+			else
+				next.setPrev(prev);
+
+			if (prev == null)
+				return removeFirst();
+			else
+				prev.setNext(next);
+
+			size--;
+
+			return node.getData();
+		}
 	}
 
 	@Override
 	public T remove(T data) {
-		// TODO Auto-generated method stub
+		if (head == null)
+			return null;
+
+		for (Node<T> node = head; node != null; node = node.getNext()) {
+			if (node.getData().equals(data)) {
+				return remove(node);
+			}
+		}
+
 		return null;
 	}
 
 	@Override
 	public void eliminateAll() {
-		// TODO Auto-generated method stub
-		
+		if (head == null)
+			return;
+
+		for (Node<T> node = head; node != null; node = node.getNext()) {
+			node.setData(null);
+			remove(node);
+		}
 	}
 
 	@Override
@@ -165,12 +253,11 @@ public class MyLinkedList<T> implements MyList<T> {
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		Node<T> temp = head;
-		while (temp != null){
+		while (temp != null) {
 			sb.append(temp.getData() + " ");
 			temp = temp.getNext();
 		}
 		return "[" + sb.toString().trim() + "]";
 	}
-	
 
 }
